2c0eaef69d47bb1c58d1c889d0ed2c95921ec170
zimmer <zimmer.hao@gmail.com>
2015-10-12 01:27:00 +0800

    flask restful

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-@app.route('/')
-def index():
-    return 'Index Page'
-
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    return 'User %s' % username
-
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-
-@app.route('/hello')
-def hello_world():
-    return 'hello world'
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0')
+    app.run('0.0.0.0', debug=True)
 
 
 
