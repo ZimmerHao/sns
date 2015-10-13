8469c69b0a357a404419e7137d9a43508229cf18
zimmer.hao@gmail.com <haojinming@breadtrip.com>
2015-10-13 18:54:29 +0800

    init models

--- config.py
+++ config.py
@@ -8,9 +8,19 @@ class Default(object):
     # debug mole
     DEBUG = True
 
-    # configuration mysql--- for prodict
-    SQLALCHEMY_DATABASE_URI = None
+    # postgresql configuration
+    DATABASE = {
+        'NAME': 'testsns',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
 
+    SQLALCHEMY_DATABASE_URI = Config.get_database_uri(**DATABASE)
+    SQLALCHEMY_BINDS = {
+
+    }
     # the secret key
     SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     # SECRET_KEY = "secret"
@@ -19,3 +29,15 @@ class Default(object):
     # celery configuration
     CELERY_BROKER_URL = 'redis://localhost:6379'
     CELERY_IMPORTS = ['tasks']
+
+
+class Config(object):
+    @staticmethod
+    def get_database_uri(**database):
+        return 'postgresql://{user}:{password}@{host}:{port}/{name}'.format(
+            user=database['USER'],
+            password=database['PASSWORD'],
+            host=database['HOST'],
+            port=database['PORT'],
+            name=database['NAME'],
+        )
--- sns/models/models.py
+++ sns/models/models.py
@@ -2,3 +2,82 @@
 __author__ = 'jinming'
 
 
+from extensions import db
+
+
+class Endpoint(db.Model):
+    """
+    This model stores the information of endpoint for the app and mobile device.
+    ===============
+    For IOS endpoint:
+    device_type: IOS=0
+    device_token: the token get from apple.
+    endpoint_arn: the amazon resouce name of the endpoint, this arn get from amazon.
+    ===============
+    For IOS endpoint:
+    device_type: ANDROID=1
+    device_token: the token get from baidu.
+    endpoint_arn: the amazon resouce name of the endpoint, this arn get from amazon.
+    """
+    __tablename__ = 'endpoint'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    device_type = db.Column(db.Integer)
+    device_token = db.Column(db.String(100), unique=True)
+    endpoint_arn = db.Column(db.String(200), unique=True)
+    is_active = db.Column(db.Boolean())
+    date_added = db.Column(db.DateTime())
+
+    topic_endpoints = db.relationship('TopicEndpoint', backref='endpoint', lazy='dynamic')
+
+
+
+
+class Topic(db.Model):
+    """
+    This model stores all topics in system. One topic is one group,
+    you can put any endpoint you want to a topic, then these endpoints
+    are in the group.
+    """
+    __tablename__ = 'topic'
+    id = db.Column(db.Integer, primary_key=True)
+    topic_name = db.Column(db.String(20), primary_key=True)
+    topic_lang = db.Column(db.String(20))
+    topic_arn = db.Column(db.String(200), unique=True)
+    is_active = db.Column(db.Boolean)
+    date_added = db.Column(db.DateTime)
+
+
+
+
+class TopicEndpoint(db.Model):
+    """
+    This model stores the detail information of subscribed topic of every endpoint,
+    one endpoint maybe in several topics.
+    """
+    __tablename__ = 'topic_endpoint'
+    id = db.Column(db.Integer, primary_key=True)
+    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
+    endpoint_id = db.Column(db.Integer, db.ForeignKey('endpoint.id'))
+    subscribe_arn = db.Column(db.String(200), unique=True)
+    is_active = db.Column(db.Boolean)
+    date_added = db.Column(db.DateTime)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
